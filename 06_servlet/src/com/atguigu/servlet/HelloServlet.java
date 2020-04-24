869657bebad41e7a79067b01e3cfcff8438fda91
lwl <525478637@qq.com>
2020-04-24 15:15:34 +0800

    15：15

--- 06_servlet/src/com/atguigu/servlet/HelloServlet.java
+++ 06_servlet/src/com/atguigu/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.atguigu.servlet;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 public class HelloServlet implements Servlet {
@@ -12,6 +13,15 @@ public class HelloServlet implements Servlet {
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
         System.out.println("2 init初始化方法");
+
+        //可以获取Servlet程序的别名servlet-name的值
+        System.out.println("HelloServlet程序的别名是："+servletConfig.getServletName());
+        //获取初始化参数init-param
+        System.out.println("初始化参数username的值是："+servletConfig.getInitParameter("username"));
+        System.out.println("初始化参数username的值是："+servletConfig.getInitParameter("url"));
+        //获取ServletContext对象
+        //org.apache.catalina.core.ApplicationContextFacade@14d22a2
+        System.out.println(servletConfig.getServletContext());
     }
 
     @Override
@@ -24,8 +34,33 @@ public class HelloServlet implements Servlet {
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
         System.out.println(" 3 service=====HelloServlet 被访问了");
+        //类型转换（因为他有getMethod()方法）
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        //获取请求方式
+        String method = httpServletRequest.getMethod();
+        //System.out.println(method);
+        if ("GET".equals(method)){
+            //System.out.println("get请求");
+            doGet();
+        }else if ("POST".equals(method)){
+           // System.out.println("post请求");
+            doPost();
+        }
     }
-
+    //做get请求的操作
+   public void doGet(){
+       System.out.println("get请求");
+       System.out.println("get请求");
+       System.out.println("get请求");
+       System.out.println("get请求");
+       System.out.println("get请求");
+   }
+    //做post请求的操作
+    public void doPost(){
+        System.out.println("post请求");
+        System.out.println("post请求");
+        System.out.println("post请求");
+    }
     @Override
     public String getServletInfo() {
         return null;
---
+++ 06_servlet/src/com/atguigu/servlet/HelloServlet2.java
@@ -0,0 +1,40 @@
+package com.atguigu.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet2 extends HttpServlet {
+
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        System.out.println("重写了init初始化方法，做了一些工作");
+    }
+
+    /*
+    * doGet()方法在get请求的时候调用
+    * */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet2的doGet方法");
+
+        //也可以使用
+        ServletConfig servletConfig = getServletConfig();
+        System.out.println(servletConfig);
+
+        System.out.println("初始化参数username的值是："+servletConfig.getInitParameter("username"));
+        System.out.println("初始化参数username的值是："+servletConfig.getInitParameter("url"));
+    }
+    /*
+     * doPost()方法在post请求的时候调用
+     * */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("HelloServlet2的doPost方法");
+    }
+}
